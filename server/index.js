1803331dbe67e02b524dfa1cc27cdd89a204b6ca
Priyam-28 <barikpriyam28@gmail.com>
2025-06-16 20:59:44 +0530

    rag implemented

--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import multer from 'multer';
 import { Queue } from "bullmq";
+import { VectorStore } from '@langchain/core/vectorstores';
+import { QdrantVectorStore } from "@langchain/qdrant";
+import { MistralAIEmbeddings } from "@langchain/mistralai";
+import { MistralAI } from "@langchain/mistralai";
+import 'dotenv/config';
 
 const app = express();
 const queue = new Queue("file-queue", {
@@ -10,6 +15,15 @@ const queue = new Queue("file-queue", {
         port: 6379
     }
 });
+
+const client = new MistralAI({
+  model: "codestral-latest",
+  temperature: 0,
+  maxTokens: undefined,
+  maxRetries: 2,
+  apiKey: process.env.MISTRAL_API_KEY,
+});
+
 app.use(cors());
 
 const storage = multer.diskStorage({
@@ -37,6 +51,50 @@ app.post('/upload/pdf', upload.single('pdf'), async (req, res) => {
     }))
     return res.json({message: 'uploaded'});
 });
+
+app.get('/chat', async (req, res) => {
+  const userQuery = "whar is the idea of the document?"; //req.query.message    ;
+
+  if (!userQuery) {
+    return res.status(400).json({ error: "Missing query parameter: message" });
+  }
+
+  try {
+    const embeddings = new MistralAIEmbeddings({
+      model: "mistral-embed",
+      apiKey: process.env.MISTRAL_API_KEY,
+    });
+
+    const vectorStore = await QdrantVectorStore.fromExistingCollection(embeddings, {
+      url: `http://localhost:6333`,
+      collectionName: "rag-t",
+    });
+
+    const ret = vectorStore.asRetriever({ k: 2 });
+    const result = await ret.invoke(userQuery);
+
+    const SYSTEM_PROMPT = `
+      You are a helpful AI assistant who answers the user query based on the available context from PDF File.
+      Context:
+      ${JSON.stringify(result)}
+    `;
+
+    const response = await client.invoke([
+      { role: 'system', content: SYSTEM_PROMPT },
+      { role: 'user', content: userQuery },
+    ]);
+
+    return res.json({
+      message: response.content,
+      docs: result,
+    });
+
+  } catch (error) {
+    console.error("Error in /chat:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 
 
 app.listen(8000, () => {
